a3e00921105a10930a4dbe24c9a29042c7d9f352
lbouriez <lbouriez@yahoo.fr>
2017-01-17 09:28:36 -0500

    Create deploy.bat

---
+++ deploy.bat
@@ -0,0 +1,11 @@
+set varServerPath=http://ldn-nt-sqldev12.eu.newedge.int/reportserver
+set varReportFolder=Spartacus/CSReports
+Set varDatasetFolder=
+set varDataSourceFolder=Spartacus/Data Sources
+Set varDataSourcePath=Spartacus/Data Sources
+set varReportName=
+set varReportFilePath=.\Reports
+
+
+echo Deployment
+rs.exe -i ssrsDeployScript.rss -s %varServerPath% -v ReportFolder="%varReportFolder%" -v DataSetFolder="%varDatasetFolder%" -v DataSourceFolder="%varDataSourceFolder%" -v DataSourcePath="%varDataSourcePath%" -v ReportName="%varReportName%"  -v filePath="%varReportFilePath%" -e Mgmt2010
